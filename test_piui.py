9025124eb523e567374ef22a91d0868385c27412
David Singleton <davidsingleton@gmail.com>
2013-01-27 18:04:15 -0800

    Added click test

--- test_piui.py
+++ test_piui.py
@@ -18,7 +18,7 @@ class PiUiTestCase(unittest.TestCase):
         return handler.getcode(), handler.read()
 
     def click(self):
-        pass
+        self._clicked = True
 
     def test_menu(self):
         self.page = self._ui.new_ui_page(title="PiUi")
@@ -53,6 +53,31 @@ class PiUiTestCase(unittest.TestCase):
         assert '"cmd": "addli"' in resp[1]
         resp = self.http_get('/poll')
         assert '"cmd": "timeout"' in resp[1]
+
+    def test_clicks(self):
+        self._clicked = False
+        self.page = self._ui.new_ui_page(title="PiUi")
+        self.title = self.page.add_textbox("Buttons!", "h1")
+        plus = self.page.add_button("Test Button", self.click)
+        resp = self.http_get('/')
+        assert "initPiUi();" in resp[1]
+        resp = self.http_get('/init')
+        assert "ok" in resp[1]
+        resp = self.http_get('/poll')
+        assert '"cmd": "newpage"' in resp[1]
+        resp = self.http_get('/poll')
+        assert '"cmd": "pagepost"' in resp[1]
+        resp = self.http_get('/poll')
+        assert '"cmd": "addelement"' in resp[1]
+        resp = self.http_get('/poll')
+        assert '"cmd": "addbutton"' in resp[1]
+        btn_cmd = resp[1]
+        resp = self.http_get('/poll')
+        assert '"cmd": "timeout"' in resp[1]
+        decoder = json.JSONDecoder()
+        cmd = decoder.decode(btn_cmd)
+        self.http_get('/click?eid=' + cmd['eid'])
+        assert self._clicked
 
 
 if __name__ == '__main__':
